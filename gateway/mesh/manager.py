5f0941f10a46b7dd09598706af51b1096556443e
Minims <github@minims.fr>
2022-12-01 21:29:40 +0100

    [Format] Run black -l 120

--- gateway/mesh/manager.py
+++ gateway/mesh/manager.py
@@ -10,7 +10,7 @@ class NodeManager:
     Notice that the interface is slightly different, due to the use of UUIDs as keys.
     Therefore no inheritance from tools.Store is implemented.
     """
-    
+
     def __init__(self, store, config, types):
         self._store = store
         self._types = types
@@ -25,17 +25,17 @@ class NodeManager:
         return len(self._nodes)
 
     def _make_node(self, uuid, info, node_config=None):
-        typename = info.get('type')
+        typename = info.get("type")
 
         # check if the user changed the node type in the configuration
         # this needs to be done here, before the actual node class is instanciated
         if node_config:
-            user_typename = node_config.optional('type', typename)
+            user_typename = node_config.optional("type", typename)
             if user_typename != typename:
                 logging.warning(f'Node type changed for {uuid} from "{typename}" to "{user_typename}"')
-            
+
                 typename = user_typename
-                info['type'] = typename
+                info["type"] = typename
 
         if typename is None or typename not in self._types:
             raise Exception(f'Invalid node type "{typename}" for {uuid}')
@@ -60,7 +60,7 @@ class NodeManager:
 
     def add(self, node):
         if str(node.uuid) in self._nodes:
-            logging.warning(f'Node {node} already exists')
+            logging.warning(f"Node {node} already exists")
         self._nodes[str(node.uuid)] = node
 
     def create(self, uuid, info):
@@ -68,7 +68,7 @@ class NodeManager:
 
     def delete(self, uuid):
         del self._nodes[str(uuid)]
-        
+
     def all(self):
         return self._nodes.values()
 
--- gateway/mqtt/bridges/light.py
+++ gateway/mqtt/bridges/light.py
@@ -7,41 +7,42 @@ class GenericLightBridge(HassMqttBridge):
     """
     Generic bridge for lights
     """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     @property
     def component(self):
-        return 'light'
+        return "light"
 
     async def config(self, node):
         color_modes = set()
         message = {
-            '~': self._messenger.node_topic(self.component, node),
-            "name": node.config.optional('name'),
-            "unique_id": node.config.require('id'),
-            "object_id": node.config.require('id'),
+            "~": self._messenger.node_topic(self.component, node),
+            "name": node.config.optional("name"),
+            "unique_id": node.config.require("id"),
+            "object_id": node.config.require("id"),
             "command_topic": "~/set",
             "state_topic": "~/state",
             "schema": "json",
         }
 
         if node.supports(Light.BrightnessProperty):
-            message['brightness_scale'] = 50
-            message['brightness'] = True
+            message["brightness_scale"] = 50
+            message["brightness"] = True
 
         if node.supports(Light.TemperatureProperty):
-            color_modes.add('color_temp')
+            color_modes.add("color_temp")
             # convert from Kelvin to mireds
             # TODO: look up max/min values from device
             # message['min_mireds'] = 1000000 // 7000
             # message['max_mireds'] = 1000000 // 2000
-            
-        if color_modes:
-            message['color_mode'] = True
-            message['supported_color_modes'] = list(color_modes)
 
-        await self._messenger.publish(self.component, node, 'config', message)
+        if color_modes:
+            message["color_mode"] = True
+            message["supported_color_modes"] = list(color_modes)
+
+        await self._messenger.publish(self.component, node, "config", message)
 
     async def _state(self, node, onoff):
         """
@@ -50,23 +51,23 @@ class GenericLightBridge(HassMqttBridge):
         If the light is on, all properties are set to their retained state.
         If the light is off, properties are not passed at all.
         """
-        message = { 'state': 'ON' if onoff else 'OFF' }
+        message = {"state": "ON" if onoff else "OFF"}
 
         if onoff and node.supports(Light.BrightnessProperty):
-            message['brightness'] = node.retained(Light.BrightnessProperty, 100)
+            message["brightness"] = node.retained(Light.BrightnessProperty, 100)
         if onoff and node.supports(Light.TemperatureProperty):
-            message['color_temp'] = node.retained(Light.TemperatureProperty, 100)
+            message["color_temp"] = node.retained(Light.TemperatureProperty, 100)
 
-        await self._messenger.publish(self.component, node, 'state', message, retain=True)
+        await self._messenger.publish(self.component, node, "state", message, retain=True)
 
     async def _mqtt_set(self, node, payload):
-        if 'color_temp' in payload:
-            await node.set_mireds(payload['color_temp'])
-        if 'brightness' in payload:
-            await node.set_brightness(payload['brightness'])
-        if payload.get('state') == 'ON':
+        if "color_temp" in payload:
+            await node.set_mireds(payload["color_temp"])
+        if "brightness" in payload:
+            await node.set_brightness(payload["brightness"])
+        if payload.get("state") == "ON":
             await node.turn_on()
-        if payload.get('state') == 'OFF':
+        if payload.get("state") == "OFF":
             await node.turn_off()
 
     async def _notify_onoff(self, node, onoff):
